b2f3b4dc5efb562c46b9e2b147a23802d62f1b7f
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-13 17:06:02 +0530

    Fist Commit to push terrafomr code

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "demo-instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "Web-Server"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "region_name" {
+  description = "value of AWS region Name"
+  default = "ap-south-1"
+}
+variable "ami_id" {
+  description = "value of AMI ID"
+  default = "ami-05d2d839d4f73aafb"
+}
+
+variable "instance_type" {
+  description = "value of intance type"
+  default = "t3.micro"
+}
