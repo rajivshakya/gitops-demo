828c55904cfd5a7423c13fef1fa8134296e12e89
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-13 17:38:23 +0530

    encription  Fix

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "demo-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  root_block_device {
+    encrypted = true
+  }
   tags = {
     Name = "Web-Server"
   }
